8246fcd3e13c47cc6c975d32a911662668659c64
simy404 <asimetra404@gmail.com>
2024-08-19 15:49:08 +0300

    add : Pagination

---
+++ LinguisticsAPI/Core/LinguisticsAPI.Application/DTOs/PagedResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace LinguisticsAPI.Application.RequestParameters.Common;
+
+public class PagedResponse<T>
+{
+    public int TotalCount { get; set; }
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+    public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+    public string? PreviousPageUrl { get; set; }
+    public string? NextPageUrl { get; set; }
+    public IEnumerable<T> Items { get; set; }
+}
---
+++ LinguisticsAPI/Core/LinguisticsAPI.Application/RequestParameters/Pagination.cs
@@ -0,0 +1,7 @@
+﻿namespace LinguisticsAPI.Application.RequestParameters;
+
+public record Pagination
+{
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+}
