683e0d4ee8286de7006e5353df37ff2e0e1fbcd0
simy404 <asimetra404@gmail.com>
2024-08-23 15:11:33 +0300

    add: Identity migration

---
+++ LinguisticsAPI/Presentation/LinguisticsAPI.API/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinguisticsAPI.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UserController : ControllerBase
+	{
+	}
+	
+	public Task<IActionResult> Create([FromBody] UserCreateVM user)
+	{
+		return Ok();
+	}
+
+}
