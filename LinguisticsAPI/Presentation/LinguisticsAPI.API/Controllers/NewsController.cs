0d7e9d1995e2b3a89de743a47985815596d4a2a4
simy404 <asimetra404@gmail.com>
2024-09-02 15:07:24 +0300

    add: NewsController.cs ,News viewmodels and profile

--- LinguisticsAPI/Presentation/LinguisticsAPI.API/Controllers/NewsController.cs
+++ LinguisticsAPI/Presentation/LinguisticsAPI.API/Controllers/NewsController.cs
@@ -1,17 +1,33 @@
 ﻿using AutoMapper;
-using Microsoft.AspNetCore.Authorization;
+using LinguisticsAPI.Application.Abstraction.News;
 using Microsoft.AspNetCore.Mvc;
+
+namespace LinguisticsAPI.API.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
 public class NewsController : ControllerBase
 {
     private readonly IMapper _mapper;
+    private readonly IConfiguration _configuration;
+    private readonly INewsService _newsService;
     
-    public NewsController(IMapper mapper)
+    public NewsController(IMapper mapper, IConfiguration configuration, INewsService newsService)
     {
         _mapper = mapper;
+        _configuration = configuration;
+        _newsService = newsService;
     }
     
+    [HttpGet]
+    public async Task<ActionResult> GetAll(string? languageCode)
+    {
+        return Ok(_newsService.GetAllNews(languageCode ?? _configuration["DefaultLanguage"]));
+    }
     
+    [HttpGet("{id}")]
+    public async Task<ActionResult> Get(Guid id, string? languageCode)
+    {
+        return Ok(_newsService.GetNewsById(id, languageCode ?? _configuration["DefaultLanguage"]));
+    }
 }
