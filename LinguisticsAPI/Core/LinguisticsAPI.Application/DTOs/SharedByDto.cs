f7ccd0ee6a90bfc9da00dccf7c339ba1d0066315
simy404 <asimetra404@gmail.com>
2024-09-04 15:55:20 +0300

    add: SharedByDto.cs

---
+++ LinguisticsAPI/Core/LinguisticsAPI.Application/DTOs/SharedByDto.cs
@@ -0,0 +1,9 @@
+﻿namespace LinguisticsAPI.Application.DTOs;
+
+public class SharedByDto
+{
+    public Guid Id { get; set; }
+    public string? ProfilePicture { get; set; }
+    public string? FullName { get; set; }
+    public string Email { get; set; }
+}
