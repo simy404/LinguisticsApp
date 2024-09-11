59230867212af93a805084875e369cbb96d86bcb
simy404 <112395338+simy404@users.noreply.github.com>
2024-09-11 11:05:14 +0300

    add: AuthValidator.cs, LinkTopicValidator.cs

---
+++ LinguisticsAPI/Core/LinguisticsAPI.Application/Validators/Auth/AuthValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using LinguisticsAPI.Application.ViewModel.User;
+
+namespace LinguisticsAPI.Application.Validators.Auth;
+
+public class AuthValidator : AbstractValidator<AuthLoginVM>
+{
+    public AuthValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Invalid email format");
+        
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required")
+            .MinimumLength(6).WithMessage("Password must be at least 6 characters long");
+    }
+}
---
+++ LinguisticsAPI/Core/LinguisticsAPI.Application/Validators/LinkTopic/LinkTopicValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using LinguisticsAPI.Application.ViewModel.LinkTopic;
+
+namespace LinguisticsAPI.Application.Validators.LinkTopic;
+
+public class LinkTopicValidator : AbstractValidator<LinkTopicCreateVM>
+{
+    public LinkTopicValidator()
+    {
+        RuleFor(x => x.Title).NotEmpty().WithMessage("Title is required");
+        RuleFor(x => x.Links).NotEmpty().WithMessage("Links are required");
+    }
+}
