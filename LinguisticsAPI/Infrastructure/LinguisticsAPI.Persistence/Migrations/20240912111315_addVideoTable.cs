8537c05e6832bb6426f4ab6cca958a044756f967
simy404 <112395338+simy404@users.noreply.github.com>
2024-09-12 14:45:11 +0300

    add: migrations

---
+++ LinguisticsAPI/Infrastructure/LinguisticsAPI.Persistence/Migrations/20240912111315_addVideoTable.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LinguisticsAPI.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class addVideoTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VideoTopics",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VideoTopics", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VideoLinks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Format = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    VideoTopicId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VideoLinks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VideoLinks_VideoTopics_VideoTopicId",
+                        column: x => x.VideoTopicId,
+                        principalTable: "VideoTopics",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VideoLinks_VideoTopicId",
+                table: "VideoLinks",
+                column: "VideoTopicId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VideoLinks");
+
+            migrationBuilder.DropTable(
+                name: "VideoTopics");
+        }
+    }
+}
